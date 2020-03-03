3d75e4afbc8090255357cfdf88715c193a403445
Nick Moore <nick.moore@columbia.com>
2020-03-03 11:14:59 -0800

    init commit

---
+++ dbrx_library/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbrx_library',
+    version='0.0.1',
+    author='Your Name',
+    author_email='yourname@example.com',
+    url='https://your.repo/dbrx_library',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.7.0',
+)
